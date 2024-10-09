98f2273605102a457211b7a18340bf2083ff0c05
vaibhav <vaibhavginnalwar@gmail.com>
2024-10-09 17:52:39 +0530

    add changedetection strategy

--- angular18Pro/src/app/Components/add-employee/add-employee.component.ts
+++ angular18Pro/src/app/Components/add-employee/add-employee.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject, signal } from '@angular/core';
+import { EmployeeService } from '../../Service/employee.service';
 
 @Component({
   selector: 'app-add-employee',
   standalone: true,
   imports: [],
   templateUrl: './add-employee.component.html',
-  styleUrl: './add-employee.component.css'
+  styleUrl: './add-employee.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush  // stop change detection    //  in this case we use ChangeDetectorRef
 })
 export class AddEmployeeComponent {
 
+  empService = inject(EmployeeService);
+  // userList: any[]=[];
+  name: string = 'Html';
+
+  userList = signal<any[]>([])
+
+  constructor(private cdRef:ChangeDetectorRef){
+
+  }
+
+  ngOnInit(): void {
+    this.empService.getUsers().subscribe((res:any)=>{
+      this.userList.set(res);
+      this.name = "JAVA";
+    })
+  }
 }
---
+++ angular18Pro/src/app/Service/employee.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { MasterService } from './master/master.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  constructor(private master: MasterService) { }
+
+
+
+
+
+  getUsers() {
+    return this.master.get('https://jsonplaceholder.typicode.com/users');
+  }
+
+
+
+
+
+}
