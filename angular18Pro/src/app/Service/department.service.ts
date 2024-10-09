64f6207f693e649e6b1e43acd2517a5d6e6c5dfc
vaibhav <vaibhavginnalwar@gmail.com>
2024-10-09 16:20:03 +0530

    add constant file

--- angular18Pro/src/app/Service/department.service.ts
+++ angular18Pro/src/app/Service/department.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Constant } from '../Constant/constant';
 
 @Injectable({
   providedIn: 'root'
@@ -8,17 +9,19 @@ export class DepartmentService {
 
 
 
-  apiUrl: string = "https://projectapi.gerasim.in/api/Complaint/"
+  // apiUrl: string = "https://projectapi.gerasim.in/api/Complaint/"
 
   constructor(private http: HttpClient) { }
 
   getAllDept() {
-    return this.http.get(`${this.apiUrl}GetParentDepartment`)
+    // return this.http.get(`${Constant.API_URL}GetParentDepartment`)
+    return this.http.get(`${Constant.API_URL}${Constant.DEPARTMENT_METHODS.GET_PARENT_DEPT}`)
   }
 
 
   saveNewDept(deptObj: any) {
-    return this.http.post(`${this.apiUrl}AddNewDepartment`, deptObj)
+    // return this.http.post(`${Constant.API_URL}AddNewDepartment`, deptObj)
+    return this.http.post(`${Constant.API_URL}${Constant.DEPARTMENT_METHODS.ADD_NEW_DEPT}`, deptObj)
   }
 
 
