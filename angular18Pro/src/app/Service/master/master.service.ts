e7931d9b04bc1c2cb7eebcb2df3fc42014e974f8
vaibhav <vaibhavginnalwar@gmail.com>
2024-10-10 11:08:35 +0530

    add generic service

--- angular18Pro/src/app/Service/master/master.service.ts
+++ angular18Pro/src/app/Service/master/master.service.ts
@@ -9,27 +9,27 @@ export class MasterService {
 
   constructor(private http: HttpClient) { }
 
-  get<T>(url: string) : Observable<T>{
+  get<T>(url: string): Observable<T> {
     return this.http.get<T>(url)
   }
 
-  getwithParam<T>(url: string,params:any,) : Observable<T>{
-    const httpParams = new HttpParams({fromObject:params});
-    const httpOptions= {
+  getwithParam<T>(url: string, params: any,): Observable<T> {
+    const httpParams = new HttpParams({ fromObject: params });
+    const httpOptions = {
       params: httpParams
     }
-    return this.http.get<T>(url,httpOptions)
+    return this.http.get<T>(url, httpOptions)
   }
 
-  post<T> (URL: string,body:any) : Observable<T>{
-    return this.http.post<T>(URL,body)
+  post<T>(URL: string, body: any): Observable<T> {
+    return this.http.post<T>(URL, body)
   }
 
-  put<T> (URL: string,body:any) : Observable<T>{
-    return this.http.put<T>(URL,body)
+  put<T>(URL: string, body: any): Observable<T> {
+    return this.http.put<T>(URL, body)
   }
 
-  delete<T> (URL: string) : Observable<T>{
+  delete<T>(URL: string): Observable<T> {
     return this.http.delete<T>(URL)
   }
 }
--- angular18Pro/src/app/app.config.ts
+++ angular18Pro/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -7,3 +7,6 @@ import { provideHttpClient } from '@angular/common/http';
 export const appConfig: ApplicationConfig = {
   providers: [ provideHttpClient() , provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
+
+
+//provideExperimentalZonelessChangeDetection()  // remove zone base change detection when we provide this in providers
